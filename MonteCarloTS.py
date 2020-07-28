c28bf7cfb282dd59b3d404a1bdb0163b167d9695
Saqib Ali <saqibsali00@gmail.com>
2020-07-28 18:38:36 -0400

    Finished TreeNode, State and MonteCarloTreeSearch but code needs to be refactored to be reusable for other games

--- MonteCarloTS.py
+++ MonteCarloTS.py
@@ -1,21 +1,51 @@
-from typing import Callable
-from TreeNode import TreeNode
+from chess import Board, Move
+from TreeNode import TreeNode, State
 import numpy as np
+from model import CNNModel
+from typing import Union, Tuple, Optional
+from random import choices
+
+C_PUCT = 1.0
+LETTER_MAP = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+NUMBER_MAP = {"1": 0, "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7}
 
 
 class MonteCarloTS:
-    def __init__(self, initial_state, neural_net):
+    curr: TreeNode
+    root: TreeNode
+
+    def __init__(self, initial_state, neural_net: CNNModel):
         self.root = TreeNode(initial_state)
+        self.root.N_num_visits += 1
         self.curr = self.root
         self.visited = set()
         self.nnet = neural_net
 
-    def get_best_action(self, node: TreeNode):
-        Q = node.get_Q()
-        P = node.P_init_policy
+    def get_best_action(self, node: TreeNode, training: bool = True) -> Optional[Move]:
+        if not training:
+            max_N, best = -float("inf"), None
+            for move in node.children:
+                n = node.children[move].N_num_visits
+                if n > max_N:
+                    max_N = n
+                    best = node.children[move]
+            return best
+        else:
+            move_lst, prob = self.get_improved_policy(node)
+            return choices(move_lst, weights=prob)[0]
 
     def search(self):
-        self._search(self.curr)
+        for _ in range(40):
+            self._search(self.curr)
+        best = self.get_best_action(self.curr, training=True)
+        self.curr = self.curr.children[best]
+        return best
+
+    def enemy_move(self, move: Move):
+        if move in self.curr:
+            self.curr = self.curr.children[move]
+            if self.curr.N_num_visits == 0:
+                self.curr.N_num_visits += 1
 
     def _search(self, curr: TreeNode):
         reward = curr.state.is_end()
@@ -24,15 +54,61 @@ class MonteCarloTS:
 
         if curr not in self.visited:
             curr.P_init_policy, value = self.feed_network(curr)
+            curr.W_state_val = value
+            curr.N_num_visits += 1
             self.visited.add(curr)
             return -1 * value
         else:
-            for action in curr.state.get_actions():
-                # initialize any non explored nodes at this point (with W = 0 and N = 0)
-                # But don't add them to visited nodes
-                pass
+            selected_child, max_u = None, -float("inf")
+            poss_actions = curr.state.get_actions()
+            sum = np.sqrt(curr.N_num_visits - 1)
 
-    def feed_network(self, curr) -> tuple:
+            for action in curr.state.get_actions():
+                if action in curr.children:
+                    node = curr.children[action]
+                    u = node.get_Q() + C_PUCT * curr.state.get_policy(action) * (sum/(1 + node.N_num_visits))
+                else:
+                    # initialize any non explored nodes at this point (with W = 0 and N = 0)
+                    # But don't add them to visited nodes
+                    node = curr.state.transition_state(action)
+                    curr.children[action] = node
+                    u = node.get_Q() + C_PUCT * curr.state.get_policy(action) * (sum/(1 + node.N_num_visits))
+
+                if u > max_u:
+                    u = max_u
+                    selected_child = node
+            value = self._search(selected_child)
+            curr.N_num_visits += 1
+            curr.W_state_val += value
+            return -value
+
+    def get_improved_policy(self, curr: TreeNode, include_empty_spots: bool = False) -> Union[Tuple[list, list], np.ndarray]:
+        array = np.zeros(4096)
+        sum = curr.N_num_visits - 1
+        move_lst = []
+        probab = []
+        for move in curr.children:
+            policy = curr.children[move].N_num_visits/sum
+            move_lst += [move]
+            probab += [policy]
+            array[self.sans_to_index(move.from_square, move.to_square)] = policy
+
+        if include_empty_spots:
+            return array
+        return move_lst, policy
+
+    def sans_to_index(self, from_square: str, to_square: str):
+        index1 = LETTER_MAP[from_square[0]] * 8 + NUMBER_MAP[from_square[1]]
+        index2 = LETTER_MAP[to_square[0]] * 8 + NUMBER_MAP[to_square[1]]
+        return index1 * 64 + index2
+
+    def get_policy(self, node: TreeNode, action: Move) -> float:
+        # get 64 * first square, + 0-63
+        return node.P_init_policy[self.sans_to_index(action.from_square, action.to_square)]
+
+    def feed_network(self, curr: TreeNode) -> tuple:
         pass
+
+
 
 
--- TreeNode.py
+++ TreeNode.py
@@ -1,11 +1,26 @@
 from __future__ import annotations
-from typing import Dict
+from typing import Dict, List, Optional
+from chess import Move, Board, BLACK
+import numpy as np
+
+FEN_MAP = {"K": 6,
+           "Q": 5,
+           "B": 4,
+           "N": 3,
+           "R": 2,
+           "P": 1,
+           "k": -6,
+           "q": -5,
+           "b": -4,
+           "n": -3,
+           "r": -2,
+           "p": -1}
 
 
 class TreeNode:
-    children: dict
+    children: Dict[Move, TreeNode]
 
-    def __init__(self, state):
+    def __init__(self, state: State):
         self.state = state
         self.W_state_val = 0
         self.N_num_visits = 0
@@ -18,3 +33,82 @@ class TreeNode:
         :return: W/N or expected win at the current state.
         """
         return self.W_state_val / self.N_num_visits
+
+
+class State:
+    def __init__(self, board: Board):
+        self.board = board
+
+    def transition_state(self, action: Move) -> TreeNode:
+        board = self.board.copy()
+        board.push(action)
+        new_state = State(board)
+        return TreeNode(new_state)
+
+    def get_actions(self) -> List[Move]:
+        return list(self.board.legal_moves)
+
+    def get_representation(self) -> list:
+        board = self.board
+        state = np.zeros(64, np.uint8)
+        fen = board.fen()
+        str_i = 0
+        i = 0
+        piece_counter = 0
+
+        while piece_counter < 64:
+            c = fen[str_i]
+            if c.isnumeric():
+                for j in range(i, i + int(c)):
+                    state[j] = 0
+                piece_counter += int(c)
+
+                str_i += 1
+                i += int(c)
+            elif c in FEN_MAP:
+                state[i] = FEN_MAP[c]
+
+                str_i += 1
+                piece_counter += 1
+                i += 1
+            else:
+                # Do nothing
+                str_i += 1
+
+        bit3 = (state >> 3) & 1
+        bit2 = (state >> 2) & 1
+        bit1 = (state >> 1) & 1
+        bit0 = state & 1
+        if board.turn == BLACK:
+            turn_array = np.zeros(64, np.uint8)
+        else:
+            turn_array = np.ones(64, np.uint8)
+
+        return [bit3, bit2, bit1, bit0, turn_array]
+
+    def serialise(self) -> str:
+        to_string = self.get_representation()
+        s = ""
+        for array in to_string:
+            for num in array:
+                s += f'{num},'
+            s = s[:-1] + '\n'
+        return s
+
+    def is_end(self) -> Optional[int]:
+        if self.board.is_game_over():
+            reward = {"1-0": 2, "1/2-1/2": 0, "0-1": -2}[self.board.result()]
+            if self.board.turn == BLACK:
+                return -reward
+            return reward
+        return None
+
+    def get_policy(self, action: Move) -> float:
+        # get 64 * first square, + 0-63
+        return 0.3
+
+    def __hash__(self):
+        return (str(self.board) + str(self.board.halfmove_clock)).__hash__()
+
+    def __eq__(self, other: State):
+        return self.__hash__() == other.__hash__()
